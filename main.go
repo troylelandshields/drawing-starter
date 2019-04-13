195007e82b4892518d7eb3260a16c146f497b904
troylelandshields <troy.leland.shields@gmail.com>
2019-04-13 09:09:25 -0600

    add a main.go file

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// Using the `drawing` package (github.com/troylelandshields/drawing) to create a canvas
+
+	// Use the canvas to draw some stuff
+
+	// Don't forget to save your image!
+}
